d9d07cc262760d08f02016da87ab5f9d9eca6380
anugojuganeshbabu <38081029+anugojuganeshbabu@users.noreply.github.com>
2024-06-30 14:51:57 +0530

    Add files via upload

--- DuplicateCount.java
+++ DuplicateCount.java
@@ -1,22 +1,20 @@
 package com.test.practice;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class DuplicateCount {
 	public static void main(String[] args) {
-		String s = "manojpandey";
-		char[] c = s.toCharArray();
+		//String s = "manojpandey";
+		String s1 = "Ganesh Babu Babu Gani";
+		Set<String> set = new HashSet<>();
+		//List<String> l = Arrays.asList(s.split(""));
+		List<String> l = Arrays.asList(s1.split(" "));
 		
-		for (int i = 0; i < c.length; i++) {
-			System.out.println(c[i]);
-		}
-		List l = (List) Arrays.asList(c);
-		
-	
-	System.out.println(l);
+		String duplicates =  l.stream().filter(x->!set.add(x)).collect(Collectors.joining(","));
+		System.out.println(duplicates);
 	}
 }
